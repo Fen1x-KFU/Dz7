1750fc72ef56f7ab48c2ecb45097a2fd80b53c8a
Андрей Мингалёв <ammingalev@gmail.com>
2024-12-12 23:27:11 +0300

    Тумаков

---
+++ Тумаков/Классы и файлы/Task6.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumakov
+{
+    internal class Song
+    {
+        public string name;
+        public string author;
+        private Song prev;
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public void SetAuthor(string author)
+        {
+            this.author = author;
+        }
+
+        public void SetPrev(Song prev)
+        {
+            this.prev = prev;
+        }
+
+        // Метод для печати названия песни и ее исполнителя
+        public void Print(string song)
+        {
+            Console.WriteLine($"Песня: {song}");
+        }
+
+        public string Title(string name, string author)
+        {
+            string song = $"{name} -- {author}";
+            return song;
+        }
+
+        // Метод для сравнения двух объектов-песен
+        public override bool Equals(object obj)
+        {
+            Song other = obj as Song;
+            if (other == null)
+                return false;
+
+            return (this.name == other.name || this.author == other.author);
+        }
+    }
+}
--- Тумаков/Тумаков.cs
+++ Тумаков/Тумаков.cs
@@ -3,14 +3,56 @@ using System.ComponentModel;
 using System.Text;
 using System.IO;
 using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections;
 
 namespace Tumakov
 {
-   class Program
+    class Program
     {
         static void StartTask(string name)
         {
             Console.WriteLine($"Задание {name}\n");
+        }
+        static void CheckIFormattableImplementation(object obj)
+        {
+            if (obj is IFormattable)
+            {
+                IFormattable formattableObject = obj as IFormattable;
+                Console.WriteLine("Объект реализует интерфейс IFormattable.");
+                Console.WriteLine(obj.GetType());
+                Console.WriteLine(formattableObject);
+            }
+            else
+            {
+                Console.WriteLine("Объект НЕ реализует интерфейс IFormattable.");
+            }
+        }
+
+        static void FileFIOEmail(string fileName1, string fileName2)
+        {
+            if (File.Exists(fileName1))
+            {
+                string[] lines1 = File.ReadAllLines(fileName1);
+                string[] lines2 = new string[lines1.Length];
+
+                // Читаем файл построчно
+                for (int i = 0; i < lines1.Length; i++)
+                {
+                    int importantchar = lines1[i].IndexOf('#');
+                    string email = lines1[i].Substring(importantchar);
+                    string lineFIO = lines1[i].Substring(0, importantchar);
+
+                    lines1[i] = lineFIO;
+                    lines2[i] = email;
+                    File.WriteAllLines(fileName1, lines1);
+                    File.WriteAllLines(fileName2, lines2);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Файл не найден.");
+            }
         }
 
         static void Task1()
@@ -22,7 +64,7 @@ namespace Tumakov
 
             Console.WriteLine("Введите сумму пополнения баланса");
             string balance1 = Console.ReadLine();
-            
+
             Console.WriteLine("Введите тип банковского счёта");
             string typeaccount1 = Console.ReadLine();
 
@@ -81,7 +123,7 @@ namespace Tumakov
             if (myFile.Length > 0)
             {
                 Console.WriteLine("Файл найден. Полный путь: " + myFile[0] + "\n");
-                
+
                 // Использование относительного пути для чтения файла
                 string content = File.ReadAllText(myFile[0]);
                 File.Delete(myFile[0]);
@@ -97,12 +139,106 @@ namespace Tumakov
             }
         }
 
+        static void Task4()
+        {
+            StartTask("8.4");
+
+            // Создаем объект для проверки его реализации интерфейса IFormattable
+            object obj = "Y";
+
+            // Вызываем метод для проверки
+            CheckIFormattableImplementation(obj);
+        }
+
+        static void Task5()
+        {
+            StartTask("8.1");
+
+            string fileName1 = "Текст1.txt";
+            string fileName2 = "Текст2.txt";
+
+            string[] linefile1 = File.ReadAllLines(fileName1);
+            string[] linefile2 = File.ReadAllLines(fileName2);
+
+            FileFIOEmail(fileName1, fileName2);
+
+            string[] newlinefile1 = File.ReadAllLines(fileName1);
+            string[] newlinefile2 = File.ReadAllLines(fileName2);
+
+            Console.WriteLine("Сщдержимое 1 файла!\n");
+            for (int i = 0; i < newlinefile1.Length; i++)
+            {
+                Console.WriteLine(newlinefile1[i]);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Сщдержимое 2 файла!\n");
+            for (int i = 0; i < newlinefile2.Length; i++)
+            {
+                Console.WriteLine(newlinefile2[i]);
+            }
+            File.Delete(fileName1);
+            File.Delete(fileName2);
+            File.AppendAllLines(fileName1, linefile1);
+            File.AppendAllLines(fileName2, linefile2);
+        }
+
+        static void Task6()
+        {
+            StartTask("8.2");
+
+            Song firstSong = new Song();
+            firstSong.SetName("Тепло");
+            firstSong.SetAuthor("Макс Корж");
+
+            Song secondSong = new Song();
+            secondSong.SetName("TSM");
+            secondSong.SetAuthor("MACAN");
+
+            Song thirdSong = new Song();
+            thirdSong.SetName("Тепло"); 
+            thirdSong.SetAuthor("Елена Темникова"); 
+
+            Song fourthSong = new Song();
+            fourthSong.SetName("Без названия"); 
+            fourthSong.SetAuthor("MACAN"); 
+
+            List<Song> list = new List<Song>(); // Создаем список песен
+
+            // Добавляем песни в список
+            list.Add(firstSong);
+            list.Add(secondSong);
+            list.Add(thirdSong);
+            list.Add(fourthSong);
+
+            // Выводим автора для каждой песни из списка
+            for (int i = 0; i < list.Count; i ++)
+            {
+                list[i].Print(list[i].Title(list[i].name, list[i].author));
+
+                if (i == 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Сравним 1 и 2 песню!");
+                    if (list[i].Equals(list[1]))
+                    {
+                        Console.WriteLine("У этих песен есть схожесть!\n");
+                    }
+                    else { Console.WriteLine("Эти песни разные!\n"); }
+                }
+            }   
+        }
+
         static void Main()
         {
             //Task1();
             //string str;
             //Console.WriteLine(Task2(out str));
             //Task3();
+            //Task4();
+            //Task5();
+            Task6();
         }
     }
 }
